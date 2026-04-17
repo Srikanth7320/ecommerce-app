64c2286dab2421e1aa12254559f8122af4717bc8
srikanth <venkatasrikanthkv@gmail.com>
2026-04-17 21:44:33 +0530

    Add product issue resolved

--- 2-Tier-App/backend/src/services/productService.js
+++ 2-Tier-App/backend/src/services/productService.js
@@ -1,9 +1,21 @@
-let products = require("../data/products.json");
+const fs = require("fs");
+const path = require("path");
 
-exports.getAllProducts = () => products;
+const filePath = path.join(__dirname, "../data/products.json");
+
+exports.getAllProducts = () => {
+  const data = fs.readFileSync(filePath);
+  return JSON.parse(data);
+};
 
 exports.addProduct = (product) => {
+  const data = fs.readFileSync(filePath);
+  const products = JSON.parse(data);
+
   product.id = Date.now();
   products.push(product);
+
+  fs.writeFileSync(filePath, JSON.stringify(products, null, 2));
+
   return product;
 };
